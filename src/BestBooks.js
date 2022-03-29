f3f170c818790b1ad5d7432f3b3fe2f2e6210bdd
Ella Svete <ella.svete@gmail.com>
2022-03-28 20:18:43 -0700

    finished carousel

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,4 +1,8 @@
+import axios from 'axios';
 import React from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+
+let SERVER = process.env.REACT_APP_SERVER;
 
 class BestBooks extends React.Component {
   constructor(props) {
@@ -8,6 +12,22 @@ class BestBooks extends React.Component {
     }
   }
 
+  getBooks = async () => {
+    try {
+      let results = await axios.get(`${SERVER}/books`);
+      this.setState({
+        books: results.data
+      })
+    } catch (error) {
+      console.log('we have an error:')
+      
+    }
+  }
+
+  componentDidMount = async () => {
+    
+    this.getBooks();
+  }
   /* TODO: Make a GET request to your API to fetch books for the logged in user  */
 
   render() {
@@ -18,8 +38,22 @@ class BestBooks extends React.Component {
       <>
         <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
 
-        {this.state.books.length ? (
-          <p>Book Carousel coming soon</p>
+        {this.state.books.length > 0 ? (
+          <Carousel variant="dark">
+            {this.state.books.map(book =>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src="https://place-hold.it/300x500"
+                  alt="First slide"
+                />
+                <Carousel.Caption>
+                  <h1>Title: {book.title}</h1>
+                  <p>Description: {book.description}</p>
+                </Carousel.Caption>
+              </Carousel.Item>
+            )}
+          </Carousel>
         ) : (
           <h3>No Books Found :(</h3>
         )}
